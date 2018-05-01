0ea57db9e6e2ae2a26e40e375269f20317e0557e
candraseptia <candraseptia93@gmail.com>
2018-05-01 17:04:58 +0700

    Import

---
+++ tugas.php
@@ -0,0 +1,8 @@
+<?php
+$nama = "Candra Septia Cahya";
+$matkul = "Web Programming 1";
+
+echo "NAMA : " . $nama . "<br>";
+echo "MATA KULIAH : " . $matkul . "<br>";
+
+?>
---
+++ variabel.php
@@ -0,0 +1,19 @@
+<?php
+
+$nama = "Candra Septia";
+$usia = 20;
+$honor = 10.5;
+$statusMenikah = true;
+$kosong = null;
+
+var_dump($nama); echo "<br>";
+var_dump($usia); echo "<br>";
+var_dump($honor); echo "<br>";
+var_dump($statusMenikah); echo "<br><br>";
+var_dump($kosong); echo "<br><br>";
+
+$statusMenikah = "Belum Menikah";
+echo "status menikah : $statusMenikah <br>";
+var_dump($statusMenikah); echo "<br>";
+
+?>
